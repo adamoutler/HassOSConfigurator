3e9a192d28cf61e3b242757d7238afee4b3ebe20
Adam Outler <adamoutler@adamoutler.com>
2021-01-03 23:49:30 -0500

    adjusting to get response of script

--- HassOsEnableSSH/run.sh
+++ HassOsEnableSSH/run.sh
@@ -16,7 +16,7 @@ set +e
     partition=$1
     mount /dev/$partition /tmp/$partition 2>/dev/null
     if [ -e /tmp/$partition/config.txt ]; then
-      if [ grep "$key" /tmp/$partition/config.txt ]; then
+      if [ $(grep "$key" /tmp/$partition/config.txt) ]; then
         echo "already added this key to $partition";
         return;
       fi;
