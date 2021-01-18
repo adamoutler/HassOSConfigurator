54032330f77629642aa5811df26bae6a7c9f4ce5
root <root@a0d7b954-ssh.local.hass.io>
2021-01-18 16:51:46 -0500

    run a command in a container

--- Run/run.sh
+++ Run/run.sh
@@ -1,11 +1,11 @@
-
-#!/usr/bin/with-contenv bashio
+#!/bin/bash
 
 PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 command=$(cat options.json |jq -r '.command');
+container=$(cat options.json |jq -r '.container');
 echo "executing";
-$command;
+docker exec  $(docker container ls|grep "$container"|awk '{print $1}') \'$command\';
 
 echo;echo;echo;echo;
 echo "----DONE----  sleeping...";
