d062ffa337b204095792fd2963339a710c1519ed
root <root@a0d7b954-ssh.local.hass.io>
2021-01-18 20:01:36 -0500

    more work on runer

--- Run/run.sh
+++ Run/run.sh
@@ -11,7 +11,7 @@ set +e
 echo " Switching to Home Assistant environment and execting script at \"/config/startup/runme.sh\""
 mkdir -p /config/startup
 
-test ! docker container ls 2>&1 >/dev/null && echo "you must disable protection mode" && exit
+#test ! docker container ls 2>&1 >/dev/null && echo "you must disable protection mode" && exit
 test ! -e /config/startup/runme.sh && echo "echo this is the example script \nenv\nls /\n echo you can find the example in /config/startup/runme.sh and modify it for your own purposes">/config/startup/runme.sh
 docker exec -t $(docker container ls|grep "$container"|awk '{print $1}') bash /config/startup/runme.sh
 set -e
