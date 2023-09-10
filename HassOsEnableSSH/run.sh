083b2f6c8aa27bd12c1aba1f685455cde8b01e89
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2023-09-10 10:36:27 -0700

    Code cleanup

--- HassOsEnableSSH/run.sh
+++ HassOsEnableSSH/run.sh
@@ -1,40 +1,52 @@
 #!/usr/bin/with-contenv bashio
 
-fun() {  while true; do nc -l -p 8099 -e  echo -e 'HTTP/1.1 200 OK\r\nServer: DeskPiPro\r\nDate:$(date)\r\nContent-Type: text/html; charset=UTF8\r\nCache-Control: no-store, no cache, must-revalidate\r\n\r\n<!DOCTYPE html><html><body><p>This addon gains 2 security points for implementing this page. So it is here.</body></html>\r\n\n\n'; done }; fun&
-
-key=$(cat options.json |jq -r '.SSHKey')
-until false; do
 set +e
-  mkdir /tmp 2>/dev/null
-  mkdir /tmp/vda1 /tmp/mmcblk0p1 /tmp/mmcblk1p1 /tmp/sda1 /tmp/sdb1 /tmp/nvme0n1p1 2> /dev/null
-  if [ ! -e /dev/sda1 ] && [ ! -e /dev/vda1 ] && [ ! -e /dev/mmcblk0p1 ] && [ ! -e /dev/mmcblk1p1 ] && [ ! -e /dev/sdb1 ] && [ ! -e /dev/nvme0n1p1 ] ; then 
-    echo "nothing to do. I can't find a /dev/vda1, /dev/sda1, /dev/sdb1, /dev/mmcblk0p1 /dev/mmcblk1p1 or /dev/nvme0n1p1";
-    while true; do sleep 99999; done;
-  fi;
 
-  performWork () {
-    partition=$1
-    mount /dev/$partition /tmp/$partition 2>/dev/null
-    if [ -e /tmp/$partition/cmdline.txt ]; then
-      if test -e /tmp/$partition/CONFIG/ && grep "$key" /tmp/$partition/CONFIG/authorized_keys 2>&1>/dev/null; then
-        echo "already added this key to $partition";
-        return;
-      fi;
-      echo "creating authorized keys in $partition !"
-      mkdir -p /tmp/$partition/CONFIG
-      echo "$key">>/tmp/$partition/CONFIG/authorized_keys
-    else
-      echo "no $partition config found"
-    fi
-  }
+fun() { while true; do nc -l -p 8099 -e echo -e 'HTTP/1.1 200 OK\r\nServer: DeskPiPro\r\nDate:$(date)\r\nContent-Type: text/html; charset=UTF8\r\nCache-Control: no-store, no cache, must-revalidate\r\n\r\n<!DOCTYPE html><html><body><p>This addon gains 2 security points for implementing this page. So it is here.</body></html>\r\n\n\n'; done; }
+fun &
 
-  performWork vda1
-  performWork sda1
-  performWork sdb1
-  performWork mmcblk0p1
-  performWork mmcblk1p1
-  performWork nvme0n1p1
+key=$(jq -r '.SSHKey' options.json)
 
-  echo "This Configurator did it's job. Perform a hard-power-off now. This configurator only works once and is no longer needed."
-  sleep 99999;
+copyKeyToDevicePartition() {
+  partition="/dev/${1}"
+  tmp_path="/tmp/${1}"
+  config_dir="${tmp_path}/CONFIG"
+  authorized_keys_file="${config_dir}/authorized_keys"
+
+  if [ ! -e "${partition}" ]; then
+    echo "[skip] ${partition} does not exist."
+    return
+  fi
+
+  mkdir -p "${tmp_path}" 2>/dev/null
+  mount "${partition}" "${tmp_path}" 2>/dev/null
+
+  if [ ! -e "${tmp_path}/cmdline.txt" ]; then
+    echo "[skip] No config file found in ${partition}"
+    return
+  fi
+
+  if test -e "${config_dir}/" && grep "$key" "${authorized_keys_file}" >/dev/null 2>&1; then
+    echo "[skip] Key already exists in ${partition}"
+    return
+  fi
+
+  echo "Writing authorized_keys in ${partition}"
+  mkdir -p "${config_dir}"
+  echo "$key" >>"${authorized_keys_file}"
+  echo "[SUCCESS] Key written to ${partition}."
+}
+partitions=(
+  vda1
+  sda1
+  sdb1
+  mmcblk0p1
+  mmcblk1p1
+  nvme0n1p1
+)
+for partition in "${partitions[@]}"; do
+  copyKeyToDevicePartition "${partition}"
 done
+
+echo "[Done] Configurator complete. Perform a hard power-off now. This configurator only works once and is no longer needed."
+sleep 99999
